6734282c591c9d9e27ba2730ba93b17f8f02674a
Stubbs-Ian <stu15015@byui.edu>
2018-10-26 13:00:16 -0700

    Functions stubbed out

--- week06/node.h
+++ week06/node.h
@@ -34,12 +34,48 @@ namespace custom
 	public:
 		// constructors and destructors
 		Node():            data(NULL) pNext(NULL) pPrev(NULL) {};
-		Node(const T & t): data(num) pNext(NULL) pPrev(NULL)  {};
+		Node(const T & t): data(num)  pNext(NULL) pPrev(NULL)  {};
 		
+        copy(const Node <T> *pSource)      throw (const char *);
+        insert(Node <T> pNode, const T &t) throw (const char *);
+        find(Nade <T> *pHead, const T &t);
+        ostream & operator << (ostream & out, Node <T> *t);
+        void freeData(Node <T> * & head);
+        remove(const Node<T> *pNode);
+
 		T data;              
 		Node *pNext;               
 		Node *pPrev;
 	};
+
+
+    template<class T>
+    Node<T>::copy(const Node<T>* pSource) throw(const char *)
+    {
+
+    }
+    template<class T>
+    Node<T>::insert(Node<T> pNode, const T & t) throw(const char *)
+    {
+
+    }
+    template<class T>
+    Node<T>::find(Nade<T>* pHead, const T & t)
+    {
+    }
+    template<class T>
+    ostream & Node<T>::operator<<(ostream & out, Node<T>* t)
+    {
+        // TODO: insert return statement here
+    }
+    template<class T>
+    void Node<T>::freeData(Node<T>*& head)
+    {
+    }
+    template<class T>
+    Node<T>::remove(const Node<T>* pNode)
+    {
+    }
 }; // namespace custom
 
 #endif // set_H
