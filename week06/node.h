96608116c34d35b3fe3fb0bc421955d522976649
Garrett Smith <smi16166@byui.edu>
2018-10-26 22:00:16 -0600

    Added comments everywhere and optimized further insert, find, findGreater, and the testInsertionSort methods.

--- week06/node.h
+++ week06/node.h
@@ -2,9 +2,10 @@
  * Header:
  *    node
  * Summary:
- *    This class contains the notion of an node
+ *    This class contains the notion of an node and the non-member
+ *    functions that allow them to act as a linked list
  * Author
- *    Garrett Smith & Aiden Reid
+ *    Garrett Smith, Aiden Reid, Ian Stubbs
  ************************************************************************/
 
 #ifndef node_H
@@ -25,8 +26,9 @@ namespace custom
 {
 
     /****************************************************
-     * node
+     * Node
      * A class that holds stuff as part of a linked list
+	 * All variable are public as well as constructors
      ***************************************************/
     template <class T>
     class Node
@@ -35,11 +37,24 @@ namespace custom
         T data;
         Node *pNext;
         Node *pPrev;
+
         // constructors and destructors
         Node() : data(T()), pNext(NULL), pPrev(NULL) {}
         Node(const T & t) : data(t), pNext(NULL), pPrev(NULL) {}
     };
 
+	/*****************************************************
+    * Function: FIND
+    * Description: ind the Node corresponding to a given 
+	*  passed value from a given linked-list. 
+    *               
+	* Input:
+	*  Node *pHead - Pointer to start of Linked List
+	*  const T & t - reference to value trying to be found
+	*  
+	* Output:
+	*  Node * - pointer to node found, NULL otherwise
+    *****************************************************/
     template<class T>
     Node<T> * find(Node<T>* pHead, const T & t) throw (const char *)
     {
@@ -62,6 +77,21 @@ namespace custom
 		}
     }
 
+	/*****************************************************
+	* Function: FIND GREATER
+	* Description: Searches a linked list of nodes to find
+	*  the first node that has a value greater than the 
+	*  value given, returns last node in list if nothing
+	*  is found.
+	*
+	* Input:
+	*  Node *pHead - pointer to start of Linked List
+	*  const T & t - reference to value being compared
+	*
+	* Output:
+	*  Node * - pointer to node found, last value in the
+	*           list otherwise
+	*****************************************************/
 	template<class T>
 	Node<T> * findGreater(Node<T>* pHead, const T & t) throw (const char *)
 	{
@@ -80,6 +110,18 @@ namespace custom
 		}
 	}
 
+	/*****************************************************
+	* Function: FREE DATA
+	* Description: Release all the memory contained in a
+	*  given linked-list
+	*
+	* Input:
+	*  Node * &pHead - Reference to pointer to start 
+	*                  of linked List
+	*
+	* Output:
+	*  void - all nodes in list have been set to NULL
+	*****************************************************/
     template<class T>
     void freeData(Node<T> * &head) throw (const char *)
     {
@@ -100,28 +142,54 @@ namespace custom
             cout << "ERROR: Can not insert due to - " << e.what() << endl;
         }
     }
+
+	/*****************************************************
+	* Function: REMOVE
+	* Description: Removes a given node from the 
+	*  linked list it is attached to. It returns a pointer
+	*  to the previous node in the list if such a node 
+	*  exists. Otherwise, it returns a pointer to the next
+	*  node.
+	*
+	* Input:
+	*  Node *pNode - Node to be removed
+	*
+	* Output:
+	*  Node * - Pointer to node before or after removed
+	*           Node
+	*****************************************************/
     template<class T>
-    Node<T> *remove(const Node<T>* pNode) throw (const char *)
+    Node<T> *remove(Node<T>* pNode) throw (const char *)
     {
         try
         {
             Node <T> * temp;
-			if (pNode->pPrev != NULL)
-				pNode->pPrev->pNext = pNode->pNext;
-			if (pNode->pNext != NULL)
-				pNode->pNext->pPrev = pNode->pPrev;
 
+			//Handles most cases but removing the first element or last element in the list
             if (pNode->pPrev != NULL)
             {
+				pNode->pPrev->pNext = pNode->pNext;
                 temp = pNode->pPrev;
-            }
-            else
-            {
-                temp = pNode->pNext;
+				if (pNode->pNext != NULL)
+				{
+					pNode->pNext->pPrev = pNode->pPrev;
+					temp = pNode->pNext;
+				}
+				return temp;
             }
 
-            pNode = NULL;
-            return temp;
+			//Handles removing the first element but not the last element in the list
+            else if (pNode->pNext != NULL)
+            {
+				pNode->pNext->pPrev = pNode->pPrev;
+                return temp = pNode->pNext;
+            }
+
+			//Handles removing the last element in the list
+			else
+			{
+				return NULL;
+			}
         }
         catch (const bad_alloc & e)
         {
@@ -129,6 +197,19 @@ namespace custom
         }
     }
 
+	/*****************************************************
+	* Function: COPY
+	* Description: Copy a linked-list. Takes a pointer to
+	*  a Node as a parameter and returns a newly created 
+	*  linked-list containing a copy of all the nodes 
+	*  below the list represented by the parameter.
+	*
+	* Input:
+	*  const Node *pNode - pointer to start of Linked List
+	*
+	* Output:
+	*  Node * - pointer to start of new list 
+	*****************************************************/
     template <class T>
     Node <T> * copy(const Node <T> * pNode) throw (const char *)
     {
@@ -158,6 +239,20 @@ namespace custom
         }
     }
 
+	/*****************************************************
+	* Function: INSERT
+	* Description: Insert a new Node into the current 
+	*  linked-list. The new node will be inserted before 
+	*  the reference node.
+	*
+	* Input:
+	*  Node *pNode - pointer to Node at which a new node
+	*                is to be inserted
+	*  const T & t - value being placed in new node
+	*
+	* Output:
+	*  Node * - pointer to new node
+	*****************************************************/
     template <class T>
     Node <T> * insert(Node <T> * pNode, const T & t) throw (const char *)
     {
@@ -186,6 +281,24 @@ namespace custom
         }
     }
 
+	/*****************************************************
+	* Function: INSERT
+	* Description: Insert a new Node into the current
+	*  linked-list. The new node will be inserted before
+	*  the reference node if the after value is false. It
+	*  will be inserted after the reference node if the 
+	*  after value is false.
+	*
+	* Input:
+	*  Node *pNode - pointer to Node at which a new node
+	*                is to be inserted
+	*  const T & t - value being placed in new node
+	*  const bool after - determines whether new Node is 
+	*                     inserted before or after pNode
+	*
+	* Output:
+	*  Node * - pointer to new node
+	*****************************************************/
     template <class T>
     Node <T> *insert(Node <T> * pNode, const T & t, const bool after) throw (const char *)
     {
@@ -217,6 +330,20 @@ namespace custom
         }
     }
 
+	/*****************************************************
+	* Function: INSERTION OPERATOR OVERLOAD
+	* Description: Give the behavior of how a linked-list
+	*  communicates with output streams, specifically how
+	*  the << operator is handled. Displays content of the
+	*  list.
+	*
+	* Input:
+	*  ostream & out - reference to ostream variable 
+	*  Node *pNode - pointer to start of linked-list
+	*
+	* Output:
+	*  ostream - output stream value
+	*****************************************************/
     template <class T>
     ostream & operator <<(ostream & out, const Node <T> * pNode) throw (const char *)
     {
--- week06/sortInsertion.h
+++ week06/sortInsertion.h
@@ -5,7 +5,7 @@
  *    This will contain just the prototype for insertionSortTest(). You may
  *    want to put other class definitions here as well.
  * Author
- *    <your names here>
+ *    Garrett Smith, Ian Stubbs, Aiden Reid
  ************************************************************************/
 
 #ifndef INSERTION_SORT_H
@@ -21,42 +21,44 @@ using namespace custom;
 template <class T>
 void sortInsertion(T array[], int num) throw (const char *)
 {
-	assert (array != NULL);
 	try
 	{
+		if (num <= 1) return;
+
 		Node <T> * n = NULL;
+
 		for (int i = 0; i < num; i++)
 		{
+			//First run, add 2 elements for findGreater method 
 			if (i == 0)
 			{
 				n = insert(n, array[i]);
+				i++;
+				(array[i] < n->data ? n = insert(n, array[i]) : insert(n, array[i], true));
 			}
 
+			//Smallest elements added at beginning
 			else if (array[i] < n->data)
 			{
 				n = insert(n, array[i]);
 			}
 
+			//Find the first node that is greater than value and add value in front
 			else
 			{
-				Node <T> *ne = findGreater(n, array[i]);
-				insert(ne, array[i], (ne->data < array[i]));
+				Node <T> *temp = findGreater(n->pNext, array[i]);
+				(array[i] < temp->data ? insert(temp, array[i]) : insert(temp, array[i], true));
 			}
 		}
-
-		if (n != NULL)
+		
+		//Add values back to array in sorted order
+		for (int i = 0; i < num; i++)
 		{
-			int i = 1;
-			array[0] = n->data;
-			
-			while (n->pNext)
-			{
-				n = n->pNext;
-				array[i] = n->data;
-				i++;
-			}
+			array[i] = n->data;
+			n = n->pNext;
 		}
 	}
+
 	catch (const bad_alloc & e)
 	{
 		cout << "ERROR: Can not find due to - " << e.what() << endl;
