278fc66312e8437f32ab289dcf8b7ae69b7877d4
Garrett Smith <smi16166@byui.edu>
2018-11-23 17:59:47 -0700

    finished quickSort and implemented merge method for sortMerge.h. Still need to implement the rest of sortMerge and none of heapSort is done. Still not many comments

--- week11/sortMerge.h
+++ week11/sortMerge.h
@@ -11,6 +11,19 @@
 #ifndef SORT_MERGE_H
 #define SORT_MERGE_H
 
+template <class T>
+void merge(T destination [], T source1[], int size1, T source2[], int size2)
+{
+	int i1 = 0, i2 = 0;
+
+	for (int iDestination = 0; iDestination < (size1 + size2); iDestination++)
+	{
+		if (i1 < size1 && (i2 == size2 || source1[i1] < source2[i2]))
+			destination[iDestination] = source1[i1++];
+		else
+			destination[iDestination] = source2[i2++];
+	}
+}
 /*****************************************************
  * SORT MERGE
  * Perform the merge sort
--- week11/sortQuick.h
+++ week11/sortQuick.h
@@ -11,13 +11,45 @@
 #ifndef SORT_QUICK_H
 #define SORT_QUICK_H
 
-/*****************************************************
- * SORT QUICK
- * Perform the quick sort
- ****************************************************/
+template <class T>
+void quickSort(T arr[], int left, int right)
+{
+	int i = left, j = right;
+	T tmp;
+	T pivot = arr[(left + right) / 2];
+
+	/* partition */
+	while (i <= j)
+	{
+		while (arr[i] < pivot)
+			i++;
+		while (arr[j] > pivot)
+			j--;
+		if (i <= j)
+		{
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+			i++;
+			j--;
+		}
+	}
+
+	/* recursion */
+	if (left < j)
+		quickSort(arr, left, j);
+	if (i < right)
+		quickSort(arr, i, right);
+}
+
+ /*****************************************************
+  * SORT QUICK
+  * Perform the quick sort
+  ****************************************************/
 template <class T>
 void sortQuick(T array[], int num)
 {
+	quickSort(array, 0, num-1);
 }
 
 
