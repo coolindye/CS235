cbf0f3b6351e12e920fd4585d1fc9030f3ffecbc
aidencompsci <aidenreid55@gmail.com>
2018-11-28 11:20:15 -0700

    should be working now, I didn't get it quite done last night

--- week11/sortHeap.h
+++ week11/sortHeap.h
@@ -11,14 +11,37 @@
 #ifndef SORT_HEAP_H
 #define SORT_HEAP_H
 
+ using namespace std;
+
+
+void trickleHeap(int arr[], int root, int l) {
+	int largest = l;
+	int left = 2*l + 1;
+	int right = 2*l + 2
+
+	if (left  < root && arr[left]  > arr[largest])
+		largest = left;
+	if (right < root && arr[right] > arr[largest])\
+		largest = right;
+	if (largest != l) {
+		swap(arr[l], arr[largest]);
+		trickleHeap(arr, root, largest);
+	}
+
+}
+
 /*****************************************************
  * SORT HEAP
  * Perform the heap sort
  ****************************************************/
 template <class T>
-void sortHeap(T array[], int num)
-{
-
+void sortHeap(T array[], int num) {
+   for (int i = num / 2 - 1; i >= 0; i--)
+   	trickleHeap(arr, num i);
+   for (int i = num - 1; i >= 0; i--) {
+   	swap(arr[0], arr[i]);
+   	trickleHeap(arr, i, 0);
+   }
 }
 
 
