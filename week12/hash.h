89371b31d7844b8f251e2123a881df4c5f98b19f
Stubbs-Ian <stu15015@byui.edu>
2018-12-07 18:57:36 -0700

    updating

--- week12/hash.h
+++ week12/hash.h
@@ -5,67 +5,88 @@
  * Author:
  *    Garrett/Aiden/Ian
  * Summary:
- *    
+ *
  ************************************************************************/
 
 #ifndef HASH_H
 #define HASH_H
 
 #include <iostream>
+#include <list>
+using namespace std;
 
 template<class T>
 class Hash
 {
 private:
-    T * table;
-    int numElements; 
+    list<T> *table;
+    int numElements;
     int numBuckets;
 
 public:
     Hash(int numBuckets) throw (const char *);
     Hash(const Hash &rhs) throw (const char *);
-    virtual ~Hash() { delete[] table; }
-
     Hash & operator = (const Hash & rhs);
-    int size() const { return numElements; }
-    int capacity() const { return numBuckets; }
-    bool empty() const { return this->numElements == 0;  }
-    bool find(const T & t) throw (const char *);
+    virtual ~Hash() {  }
 
+    int  size()     const { return numElements; }
+    int  capacity() const { return numBuckets; }
+    bool empty()    const { return numElements == 0; }
+
+    bool find(const T & t) throw (const char *);
     void insert(const T & t) throw (const char *);
 
     virtual int hash(const T & t) const = 0;
 
 };
 
-
-#endif // HASH_H
-
 template<class T>
 inline Hash<T>::Hash(int numBuckets) throw(const char *)
 {
     this->numBuckets = numBuckets;
+    this->numElements = 0;
+    this->table = new list<T>[numBuckets];
 }
 
 template<class T>
 inline Hash<T>::Hash(const Hash & rhs) throw(const char *)
 {
+    this->table = rhs.table;
+    this->numElements = rhs.numElements;
+    this->numBuckets = rhs.numBuckets;
 }
 
 template<class T>
 Hash<T> & Hash<T>::operator=(const Hash & rhs)
 {
-    // TODO: insert return statement here
+    table = rhs.table;
+    numElements = rhs.numElements;
+    numBuckets = rhs.numBuckets;
     return *this;
 }
 
 template<class T>
 inline bool Hash<T>::find(const T & t) throw(const char *)
 {
+    int index = hash(t);
+    list<T> tempList(table[index]);
+    //tempList.find(t);
+
+    for (typename list<T>::iterator it = tempList.begin(); it != tempList.end(); ++it) {
+        if (*it == t)
+            return true;
+    }
+
     return false;
 }
 
 template<class T>
 inline void Hash<T>::insert(const T & t) throw(const char *)
 {
+    numElements++;
+    int index = hash(t);
+    table[index].push_back(t);
+
 }
+
+#endif // HASH_H
