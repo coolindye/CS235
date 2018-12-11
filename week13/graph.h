6fa594f02379401561f95e26aa495a4795a628ae
Stubbs-Ian <stu15015@byui.edu>
2018-12-11 15:41:18 -0700

    so yeah, not sure if a 2D array would be the best option or not

--- week13/graph.h
+++ week13/graph.h
@@ -11,7 +11,36 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include "set.h"
+
+namespace custom
+{
+
+    template <class T>
+    class Graph
+    {
+    private:
+        int vertices;
+        int twoD;
+        bool **matrix[];
+        //bool *matrix2D[][];
+    public:
+        Graph() {}
+        Graph(int vertices) throw (const char *);
+    };
+
+    template<class T>
+    Graph<T>::Graph(int vertices) throw(const char *)
+    {
+        this->vertices = vertices;
+        /*int rowCount = vertices;
+        int colCount = vertices;
+        matrix = new bool*[rowCount];
+        for (int i = 0; i < rowCount; ++i)
+            a[i] = new bool[colCount];*/
+    }
 
 
+};
 
 #endif // GRAPH_H
